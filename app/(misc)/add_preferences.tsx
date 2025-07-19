853aa20f461fa74c4d81a3469d3824cf07a9ee70
lazroProgrammer <lamineyounes777@gmail.com>
2025-07-19 03:46:14 +0100

    nice job

--- app/(misc)/add_preferences.tsx
+++ app/(misc)/add_preferences.tsx
@@ -2,6 +2,6 @@ import { ScrollView, View } from "react-native";
 
 export default function AddPreferences(){
     return (
-        <ScrollView style={}></ScrollView>
+        <ScrollView></ScrollView>
     );
 }
